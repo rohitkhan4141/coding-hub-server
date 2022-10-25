3558ea310bed823d8e209832646664fb503e976e
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-10-25 14:03:17 +0600

    courses router added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
 });
