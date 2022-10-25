4f9ec27ccfb83e78b352cc57e6f5953b5e2da8ab
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-10-25 14:14:11 +0600

    port and json data tweeking

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 app.use(cors());
+
+const courses = require("./data/courses.json");
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
